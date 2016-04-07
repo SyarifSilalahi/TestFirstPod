22e57e88fe8e8aa3cecd3a38f5e4d146136927c7
Syarif Silalahi <syarif@pistarlabs.com>
2016-04-07 08:57:21 +0700

    Finish first step

--- Example/TestFirstPod/ViewController.swift
+++ Example/TestFirstPod/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import TestFirstPod
 
 class ViewController: UIViewController {
+    @IBOutlet var viewPulse : UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.Tap))
+        self.viewPulse.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func Tap(){
+        var pulseEffect:CALayer!
+        pulseEffect = TestFirstPod(repeatCount: 0, radius:70, position:self.viewPulse.center)
+        pulseEffect.backgroundColor = UIColor.lightGrayColor().CGColor
+        self.viewPulse.layer.insertSublayer(pulseEffect, below: self.viewPulse.layer)
     }
 
     override func didReceiveMemoryWarning() {
